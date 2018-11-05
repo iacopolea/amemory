409335f1989d6c6886ad86f5ea7eac619aa6ade5
iacopo_lea <iacopo.lea@gmail.com>
2018-11-05 10:00:35 +0100

    config

---
+++ app.config.example.js
@@ -0,0 +1,10 @@
+const FIREBASE_CONFIG = {
+  apiKey: "API_KEY",
+  authDomain: "APP_NAME.firebaseapp.com",
+  databaseURL: "https://APP_NAME.firebaseio.com",
+  projectId: "APP_NAME",
+  storageBucket: "APP_NAME.appspot.com",
+  messagingSenderId: "xxxxxxxxxxxx"
+}
+
+export  {FIREBASE_CONFIG}
--- src/js/_firebase.js
+++ src/js/_firebase.js
@@ -1,15 +1,8 @@
 import firebase from 'firebase';
+import {FIREBASE_CONFIG} from '../../app.config'
 
-const CONFIG = {
-  apiKey: "AIzaSyCNpfWQkQiuWryP8gJV9XY2sXOKfqnb8G0",
-  authDomain: "amemory-c53ee.firebaseapp.com",
-  databaseURL: "https://amemory-c53ee.firebaseio.com",
-  projectId: "amemory-c53ee",
-  storageBucket: "amemory-c53ee.appspot.com",
-  messagingSenderId: "415926222316"
-};
 
-firebase.initializeApp(CONFIG);
+firebase.initializeApp(FIREBASE_CONFIG);
 
 
 const db = firebase.firestore();
